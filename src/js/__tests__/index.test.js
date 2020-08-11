77c167c93f5d172416724f268b34c4ccf7c74052
Vladislav Kostin <v1adyank0stin@gmail.com>
2020-08-11 12:29:03 +0400

    arraybuffer

---
+++ src/js/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import ArrayBufferConverter, { getBuffer } from '../index';
+
+test('проверка правильности выполнения кода', () => {
+  const buffer = getBuffer();
+  const test = new ArrayBufferConverter();
+  test.load(buffer);
+  const received = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+  expect(received).toEqual(test.toString());
+});
+
+test('проверка типа данных', () => {
+  const buffer = getBuffer();
+  const test = new ArrayBufferConverter();
+  test.load(buffer);
+  const received = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+  expect(typeof (received)).toEqual(typeof (test.toString()));
+});
